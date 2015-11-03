f4dda684adb42da0e4abdabe2b69c2723962cad0
Maciej Piekarniak <mp347204@students.mimuw.edu.pl>
2015-11-03 12:32:33 +0100

    dodanie rzeczy z tresci, yay

---
+++ maptel_test2.cc
@@ -0,0 +1,27 @@
+#include <cassert>
+#include <cstddef>
+#include <cstring>
+#include "cmaptel"
+
+namespace {
+
+  unsigned long testuj() {
+    unsigned long id;
+
+    id = ::jnp1::maptel_create();
+    ::jnp1::maptel_insert(id, "997", "112");
+
+    return id;
+  }
+
+  unsigned long id = testuj();
+
+} // anonymous namespace
+
+int main() {
+  char tel[::jnp1::TEL_NUM_MAX_LEN + 1];
+
+  ::jnp1::maptel_transform(id, "997", tel, ::jnp1::TEL_NUM_MAX_LEN + 1);
+  assert(::std::strcmp(tel, "112") == 0);
+  ::jnp1::maptel_delete(id);
+}
