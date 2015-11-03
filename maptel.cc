1413e4a8933e7bc50c3c87304aecd25b5dfbb7b7
Maciej Piekarniak <mp347204@students.mimuw.edu.pl>
2015-11-03 13:50:20 +0100

    dodanie smieci, yay

---
+++ maptel.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <queue>
+#include "maptel.h"
+
+
+
+unsigned long maptel_create() {
+    static unsigned long counter;
+    static std::queue
+
+    return counter;
+}
+
+int main() {
+
+    return 0;
+}
