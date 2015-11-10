d0c612e8cbd502d75f4ab56f1bcb673fca512a0e
Piotr Wiśniewski <pw361286@students.mimuw.edu.pl>
2015-11-10 18:06:41 +0100

    drobne poprawki, dodany namespace - nie wiem czy powinien cos jeszcze obejmowac

--- maptel.cc
+++ maptel.cc
@@ -1,55 +1,32 @@
-/*
-  Kompilacja: g++ -o maptel maptel.cc -std=c++11 -Wall -pedantic
-  Notki dla ciebie:
-  Trzeba jeszcze wpasc na pomysl jak reprezentowac zmiany. imo dobra opcja
-  bylby vector z push_backiem i ogolnie map<int, vector<string>>. Czy jakos
-  inaczej?
-
-  PW: Nie trzeba robic zadnych vectorow. Wydaje mi sie, ze w zadaniu chodzi o to,
-  ze w przypadku mapy ktora ma np:
-  numer1 -> numer2; numer2 -> numer3; numer3 -> numer4
-  to wtedy mamy wpisac to tel_dst numer4 (jezeli pytamy o numer1)(tak samo
-  dla cykli)
-
-
-  Wykrywanie cyklu da rade zrobic liniowo (z pamiecia stala)
-  Trzeba bedzie jeszcze ogarnac "te rzeczy" w zwiazku z C. Sam kod raczej
-  nie jest trudny
-
-  uwagi:
-  dodac jakies typedefy? przykladowo na unordered_map
-*/
-
 #include <iostream>
 #include <unordered_map>
 #include <cassert>
 #include "maptel.h"
 
-#ifndef NDEBUG
-    const bool debug = true;
-#else
-    const bool debug = false;
-#endif
+namespace {
+    #ifndef NDEBUG
+        const bool debug = true;
+    #else
+        const bool debug = false;
+    #endif
 
-typedef std::unordered_map<std::string, std::string> mapvalue;
-typedef std::unordered_map<std::string, std::string>::iterator phone_change;
-typedef std::unordered_map<unsigned long, mapvalue> phone_book_map;
+    typedef std::unordered_map<std::string, std::string> mapvalue;
+    typedef std::unordered_map<std::string, std::string>::iterator phone_change;
+    typedef std::unordered_map<unsigned long, mapvalue> phone_book_map;
 
-phone_book_map&  phone_map()
-{
-    static phone_book_map ans = phone_book_map();
-    return ans;
+    phone_book_map&  phone_map()
+    {
+        static phone_book_map ans = phone_book_map();
+        return ans;
+    }
 }
 
-
-// TODO: zamienic na obsluge usunietych indeksow
 unsigned long maptel_create() {
     static unsigned long counter = 0;
     if (debug) {
         std::cerr << "maptel: maptel_create()\n";
         assert(phone_map().find(counter) == phone_map().end());
     }
-
 
     phone_map().insert(std::pair<unsigned long, mapvalue> (counter, mapvalue()));
 
@@ -61,7 +38,6 @@ unsigned long maptel_create() {
     return counter++;
 }
 
-// doned
 void maptel_delete(unsigned long id) {
     if (debug) {
         std::cerr << "maptel: maptel_delete(" << id << ")\n";
@@ -83,21 +59,17 @@ void maptel_insert(unsigned long id, char const *tel_src, char const *tel_dst) {
     if (debug) {
         std::cerr << "maptel: maptel_insert(" << id << ", " << tel_src_str
                   << ", " << tel_dst_str << ")\n";
-        assert(phone_map().find(id) != phone_map().end());
-        //assert(phone_map()[id].find(tel_src_str) != phone_map()[id].end());
     }
 
-    //wydaje mi sie ze nawet w wersji release powinnismy poinformowac uzytkownika jak
-    //uzywa niesyniejacej mapy
+    //mapa nie istnieje
     if (phone_map().find(id) == phone_map().end())
-        std::cerr << "maptel with id: " <<id <<" does not exist";
-        //w elsie mozna by dodac jakies asserty, ale to chyba byloby testowanie stla
-    else
+        std::cerr << "maptel with id: " <<id <<" does not exist\n";
+    else {
         phone_map()[id].emplace(tel_src_str, tel_dst_str);
-
-    if (debug) {
-        std::cerr << "maptel: maptel_insert: inserted\n";
-        // TODO: dodac jakas asercje
+        if (debug) {
+            std::cerr << "maptel: maptel_insert: inserted\n";
+            assert(phone_map()[id].find(tel_src) != phone_map()[id].end());
+        }
     }
 }
 
@@ -109,12 +81,14 @@ void maptel_erase(unsigned long id, char const *tel_src) {
 
     if (phone_map().find(id) == phone_map().end())
         std::cerr << "maptel with id: " <<id <<" does not exist";
-    else
-        phone_map()[id].erase(tel_src_str);
-
-
-    if (debug) {
-        std::cerr << "maptel: maptel_erase: erased\n";
+    else {
+        if (phone_map()[id].erase(tel_src_str)) {
+            if (debug)
+                std::cerr << "maptel: maptel_erase: erased\n";
+        } else {
+            if (debug)
+                std::cerr << "maptel: maptel_erase: nothing to erease\n";
+        }
     }
 }
 
@@ -128,16 +102,16 @@ void copy_number(char *dst, std::string& number, size_t max_len)
         dst[l] = '\0';
     }
 }
+
 void maptel_transform(unsigned long id, char const *tel_src, char *tel_dst, size_t len) {
         std::string tel_src_s(tel_src);
     if (debug) {
         std::cerr << "maptel: maptel_transform(" << id << ", " << tel_src_s
                   << ", " << static_cast<void*>(tel_dst) << ", " << len << ")\n";
-
     }
 
     if (phone_map().find(id) == phone_map().end())
-    std::cerr << "maptel woth id: " <<id <<" does not exist";
+        std::cerr << "maptel woth id: " <<id <<" does not exist";
     else {
         mapvalue book = phone_map()[id];
         if (book.find(tel_src_s) == book.end()) {
@@ -155,7 +129,6 @@ void maptel_transform(unsigned long id, char const *tel_src, char *tel_dst, size
                     copy_number(tel_dst, slow->second, len);
                     break;
                 }
-
                 //szybkiego przesuwamy o 2 i jak po drodze byl koniec to brak cyklu
                 if (book.find(fast->second) != book.end()) {
                     fast = book.find(fast->second);
@@ -181,32 +154,11 @@ void maptel_transform(unsigned long id, char const *tel_src, char *tel_dst, size
                         std::cerr << "maptel: maptel_transform: cycle detected\n";
                     break;
                 }
-            }
-        }
+            }//end while
+        }//end else
         if (debug) {
             std::cerr << "maptel: maptel_transform: " << tel_src_s <<" -> "
-                    << std::string(tel_dst) <<"\n";
+                    << std::string(tel_dst) <<",\n";
         }
     }
 }
-
-
-/*int main() {
-    //std::cout << "hello world";
-    char *tel1 = "1";
-    char *tel2 = "2";
-    char *tel3 = "3";
-    char *tel4 = "4";
-    char *tel5 = "5";
-    char *tel6 = "6";
-    unsigned long i = maptel_create();
-    maptel_insert(i, tel1, tel2);
-    maptel_insert(i, tel2, tel3);
-    maptel_insert(i, tel3, tel2);
-    maptel_insert(i, tel4, tel5);
-    maptel_insert(i, tel5, tel6);
-    char t[4];
-    maptel_transform(i,tel4, t, 4);
-    printf("%s", t);
-    return 0;
-}*/
